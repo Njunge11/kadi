ff77b3c4b36b245e9ba68b56e2861235206a9532
Njunge Njenga <njungedev@gmail.com>
2018-07-02 20:55:42 +0300

    remove test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const Kadi = require('./index')
-const card = new Kadi()
-const cardNumber = '4242424242424242'
-const cvc = '123'
-const expiryDate = '07/2018'
-
-console.log(card.validateCardNumber(cardNumber))
-console.log(card.validateCVC(cvc))
-console.log(card.validateExpiryDate(expiryDate))
