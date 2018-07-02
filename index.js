4f75a7b5fb788b357f9357585e49db65d31bb3ef
Njunge Njenga <njungedev@gmail.com>
2018-07-02 17:18:53 +0300

    added expiry and cvc validation

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const Kadi = require('./lib/kadi')
 const card = new Kadi()
 const cardNumber = '4242424242424242'
+const expiryDate = '07/2018'
+const cvc = undefined
 console.log(card.validateCardNumber(cardNumber))
+console.log(card.validateExpiryDate(expiryDate))
+console.log(card.validateCVC(cvc))
