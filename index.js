cbee80fe0ea92a8fe060ef59b7fe4cafa0180e37
Njunge Njenga <njungedev@gmail.com>
2018-07-02 18:46:51 +0300

    improved cvc validation

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Kadi = require('./lib/kadi')
 const card = new Kadi()
-const cardNumber = '4242424242424242'
-const expiryDate = '07/2018'
-const cvc = undefined
-console.log(card.validateCardNumber(cardNumber))
+// const cardNumber = '4242424242424242'
+// const cvc = undefined
+const expiryDate = '11/1999'
+
+// console.log(card.validateCardNumber(cardNumber))
+// console.log(card.validateCVC(cvc))
 console.log(card.validateExpiryDate(expiryDate))
-console.log(card.validateCVC(cvc))
